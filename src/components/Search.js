0bd19b6a02fa9a8f43c0aeb8daac1f6af343006a
Bettyweav1218 <bfholman415@gmail.com>
2023-02-22 21:01:36 -0600

    google clone project codes

---
+++ src/components/Search.js
@@ -0,0 +1,28 @@
+import SearchIcon from '@material-ui/icons/Search';
+import MicIcon from '@material-ui/icons/Mic';
+import { Button } from '@material-ui/core';
+import styled from 'styled-components';
+import { Search } from '@material-ui/icons';
+
+const SearchInput = styled.div` 
+    display: flex;
+    align-items: center;
+    border: 1px solid lightgray;
+    height: 30px;
+    padding: 10px 15px;
+    width: 500px;
+    margin: 0px auto;
+    margin-top: 40px;
+    border-radius: 999px;
+    input {
+        flex: 1;
+        padding: 8px 13px;
+        font-size4: medium;
+        border: 9;
+        outline: 0;
+    }
+    .searchicon {
+        color: gray;
+    }
+    `
+    export default Search;
